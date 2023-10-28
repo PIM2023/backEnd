8adfa35078732ecb3198b53d2ecd765b46e6d279
CrazyJMB <crazyjmbemail@gmail.com>
2023-10-28 18:10:09 +0200

    cambio de la ruta de login

--- src/routes/login.routes.ts
+++ src/routes/login.routes.ts
@@ -3,6 +3,6 @@ import * as loginController from "../controller/login.controller";
 
 const router = Router();
 
-router.post("/", loginController.login);
+router.get("/", loginController.login);
 
 export default router;
